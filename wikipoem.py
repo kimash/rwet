18ed20a61cdcd348e432aa6d4859d07078a6306e
Kim Ash <ash.kimberly.b@gmail.com>
2013-05-08 03:55:23 -0400

    code now uses search term input from terminal in url

--- wikipoem.py
+++ wikipoem.py
@@ -37,7 +37,9 @@ class FakeMozillaOpener(urllib.FancyURLopener):
   version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
 urllib._urlopener = FakeMozillaOpener()
 
-url = "http://en.wikipedia.org/wiki/Nabokov"
+term = sys.argv[1]
+url = 'http://en.wikipedia.org/wiki/' + term
+first_let = term[0].upper()
 
 data = urllib.urlopen(url).read()
 soup = BeautifulSoup(data)
